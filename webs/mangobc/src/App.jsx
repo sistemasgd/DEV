6e0835ff06841a823600ce4f65b23203ddb88078
sistemasgd <fdieguez@grupodiazindustrial.com>
2023-03-13 10:29:09 -0600

    first commit mango

---
+++ webs/mangobc/src/App.jsx
@@ -0,0 +1,12 @@
+import image from './assets/images/proximamente.jpg'
+import './App.css'
+
+function App () {
+  return (
+    <main>
+      <img src={image} className='image' alt='' />
+    </main>
+  )
+}
+
+export default App
