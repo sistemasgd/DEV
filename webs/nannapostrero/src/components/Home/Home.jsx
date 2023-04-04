466333d2af3b2c45cf1d7ec0f0af54c5d6d29df2
sistemasgd <fdieguez@grupodiazindustrial.com>
2023-04-04 13:28:32 -0600

    import images home

--- webs/nannapostrero/src/components/Home/Home.jsx
+++ webs/nannapostrero/src/components/Home/Home.jsx
@@ -1,61 +1,64 @@
 import './Home.css'
+import Beso from '../../assets/images/beso_nuez.png'
+import Pay from '../../assets/images/pay.png'
+import Circulo from '../../assets/images/prueba.png'
 export function Home () {
   const productos = [
     {
       id: 0,
       title: 'besos de nuez',
-      img: 'src/assets/images/beso_nuez.png',
+      img: Beso,
       descripcion: 'descripcion'
     },
     {
       id: 1,
       title: 'besos de chocolate',
-      img: 'src/assets/images/beso_nuez.png',
+      img: Beso,
       descripcion: 'descripcion'
     },
     {
       id: 2,
       title: 'pay',
-      img: 'src/assets/images/pay.png',
+      img: Pay,
       descripcion: 'descripcion'
     }
   ]
   const list = [
     {
       id: 0,
-      img: 'src/assets/images/beso_nuez.png',
+      img: Beso,
       descripcion: '¿Quieres distribuir en alguna parte de la República?'
     },
     {
       id: 1,
-      img: 'src/assets/images/beso_nuez.png',
+      img: Beso,
       descripcion: '¿Quieres vender nuestrosproductos en tu punto de venta?'
     },
     {
       id: 2,
-      img: 'src/assets/images/pay.png',
+      img: Pay,
       descripcion: '¿Quieres nuestros productos para un evento o para consumo personal?'
     }
   ]
   const list2 = [
     {
       id: 0,
-      img: 'src/assets/images/prueba.png',
+      img: Circulo,
       descripcion: 'envío gratis'
     },
     {
       id: 1,
-      img: 'src/assets/images/prueba.png',
+      img: Circulo,
       descripcion: 'tu producto esta asegurado'
     },
     {
       id: 2,
-      img: 'src/assets/images/prueba.png',
+      img: Circulo,
       descripcion: 'aceptamos tarjetas de crédito y débito'
     },
     {
       id: 3,
-      img: 'src/assets/images/prueba.png',
+      img: Circulo,
       descripcion: 'cobertura en todo méxico'
     }
   ]
@@ -84,7 +87,7 @@ export function Home () {
       <section className='flex flex-col bg-[#001468] text-white my-10'>
         <div className='grid grid-blog container m-auto px-10'>
           <div className='flex flex-col items-center p-8'>
-            <img className='max-w-[150px]' src='src/assets/images/beso_nuez.png' alt='' />
+            <img className='max-w-[150px]' src={Beso} alt='' />
           </div>
           <div className='flex flex-col justify-center gap-10 p-8'>
             <h1 className='text-4xl uppercase'>nuestra más reciente creació<span className='alter'>n</span></h1>
@@ -127,24 +130,24 @@ export function Home () {
 
       {/* section form */}
       <section className='flex flex-col bg-[#001468] text-white mt-10'>
-        <div className='container m-auto md:p-10 p-4'>
+        <div className='flex flex-col container m-auto md:p-10 p-4 gap-10'>
           <h1 className='text-7xl text-center uppercase'>¿quieres disttribuir nanná postrero?</h1>
           <form action='#' method='post'>
             <div className='grid grid-cols-2 gap-6'>
               <div className='col-span-1 lg:col-span-1 sm:col-span-1 xl:col-span-1'>
-                <label htmlFor='name' className='block uppercase leading-6 /text-gray-900'><h1>nombre</h1></label>
-                <input id='name' name='name' type='text' className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 placeholder:text-gray-400' />
+                <label htmlFor='name' className='block uppercase leading-6 /text-gray-900 text-3xl'><h1>nombre</h1></label>
+                <input id='name' name='name' type='text' className='mt-2 block w-full rounded-md border-0 p-2 text-gray-900 placeholder:text-gray-400' />
               </div>
               <div className='col-span-1 lg:col-span-1 sm:col-span-1 xl:col-span-1'>
-                <label htmlFor='mail' className='block uppercase leading-6 /text-gray-900'><h1>mail</h1></label>
+                <label htmlFor='mail' className='block uppercase leading-6 /text-gray-900 text-3xl'><h1>mail</h1></label>
                 <input id='mail' name='mail' type='mail' className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 placeholder:text-gray-400' />
               </div>
               <div className='col-span-1 lg:col-span-1 sm:col-span-1 xl:col-span-1'>
-                <label htmlFor='phone' className='block uppercase leading-6 /text-gray-900'><h1>número telefónico</h1></label>
+                <label htmlFor='phone' className='block uppercase leading-6 /text-gray-900 text-3xl'><h1>número telefónico</h1></label>
                 <input id='phone' name='phone' type='text' className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 placeholder:text-gray-400' />
               </div>
               <div className='col-span-2'>
-                <label htmlFor='message' className='block uppercase leading-6 /text-gray-900'><h1>mensaje</h1></label>
+                <label htmlFor='message' className='block uppercase leading-6 /text-gray-900 text-3xl'><h1>mensaje</h1></label>
                 <textarea id='message' name='message' rows='4' className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 placeholder:text-gray-400' />
               </div>
             </div>
