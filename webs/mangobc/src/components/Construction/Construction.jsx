aeaad9ce8e3d579d737556a371ce0e048da216cd
sistemasgd <fdieguez@grupodiazindustrial.com>
2023-06-05 16:04:20 -0600

    commit proximamente page

--- webs/mangobc/src/components/Construction/Construction.jsx
+++ webs/mangobc/src/components/Construction/Construction.jsx
@@ -4,9 +4,9 @@ export function Construction () {
   return (
     <>
       <Navbar />
-      <div className='flex flex-wrap h-[100vh] w-full bg-[#FACB22] overflow-hidden'>
-        <section className='flex items-center'>
-          Hola
+      <div className='flex h-[100vh] w-full bg-[#FACB22] overflow-hidden'>
+        <section className='flex flex-col flex-wrap justify-center w-full items-center'>
+          <img className='w-1/2' src='/images/proximamente.gif' alt='coming soon' />
         </section>
       </div>
     </>
