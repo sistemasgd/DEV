9b03668ad1ca53c03a45b22e02326797148b2a90
sistemasgd <fdieguez@grupodiazindustrial.com>
2023-06-07 16:23:12 -0600

    commit fix parallax

--- webs/mangobc/src/components/Home/Home.jsx
+++ webs/mangobc/src/components/Home/Home.jsx
@@ -10,23 +10,7 @@ export function Home () {
       <Header />
       <main>
         <Marcas />
-        <div className='parallax'>
-          <div className='flex flex-col relative top-36 gap-4'>
-            <span className='flex justify-center font-bold text-7xl text-[#FACB22] text-center uppercase texto_borde'>Línea industrial</span>
-            <span className='flex justify-center'>
-              <img className='w-12 h-12' src='images/icon.png' />
-            </span>
-          </div>
-        </div>
         <Industrial />
-        <div className='parallax2'>
-          <div className='flex flex-col relative top-36 gap-4'>
-            <span className='flex justify-center font-bold text-7xl text-[#FACB22] text-center uppercase texto_borde'>Línea institucional</span>
-            <span className='flex justify-center'>
-              <img className='w-12 h-12' src='images/icon.png' />
-            </span>
-          </div>
-        </div>
         <Institucional />
         <Contacto />
       </main>
