4f863e2b757343648cc0793466a0cfc03ba8206b
sistemasgd <fdieguez@grupodiazindustrial.com>
2023-09-05 12:02:58 -0600

    add distribution form for mkt

--- webs/mango/src/data/email.js
+++ webs/mango/src/data/email.js
@@ -3,3 +3,9 @@ export const KEY = {
     TEMPLATE_ID: 'template_0d9dy0x',
     USER_ID: 'vts7MSfwkYZG1ytM_'
 }
+
+export const DISTRIBUTION_KEY = {
+    SERVICE_ID: 'service_56lnfoy',
+    TEMPLATE_ID: 'template_uj83w2f',
+    USER_ID: 'vts7MSfwkYZG1ytM_'
+}
