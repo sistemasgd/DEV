9d8e30c8baf6727234e4d6df6fb15f122713776e
sistemasgd <fdieguez@grupodiazindustrial.com>
2023-09-07 12:46:09 -0600

    add images to job slider

--- webs/mango/src/components/jobslider/JobSlider.jsx
+++ webs/mango/src/components/jobslider/JobSlider.jsx
@@ -5,13 +5,16 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
+import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
 
 import './style.css'
 
+import { sliders } from './SliderImages';
+
 // import required modules
-import { FreeMode, Navigation, Thumbs, Autoplay} from 'swiper';
+import { EffectFade ,FreeMode, Navigation, Thumbs, Autoplay} from 'swiper';
 
 export default function App() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
@@ -24,37 +27,25 @@ export default function App() {
           '--swiper-pagination-color': '#facb22',
         }}
         spaceBetween={10}
+        effect={'fade'}
         loop={true}
         navigation={true}
         thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs, Autoplay]}
+        modules={[EffectFade, FreeMode, Navigation, Thumbs, Autoplay]}
         autoplay={{
-          delay: 2000,
+          delay: 3500,
           disableOnInteraction: false
         }}
         className="mySwiper2"
       >
-        <SwiperSlide>
-          <img className='aspect-[750/460] max-w-[800px]' src="../images/jobs/slider/slider1.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='aspect-[750/460] max-w-[800px]' src="../images/jobs/slider/slider2.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='aspect-[750/460] max-w-[800px]' src="../images/jobs/slider/slider3.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='aspect-[750/460] max-w-[800px]' src="../images/jobs/slider/slider4.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='aspect-[750/460] max-w-[800px]' src="../images/jobs/slider/slider5.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='aspect-[750/460] max-w-[800px]' src="../images/jobs/slider/slider6.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='aspect-[750/460] max-w-[800px]' src="../images/jobs/slider/slider7.webp" alt='equipo mangobc' />
-        </SwiperSlide>
+        {
+          sliders.map((img) => (
+            <SwiperSlide key={img.id}>
+              <img className='max-w-[800px]' src={img.image} alt='equipo mangobc' />
+            </SwiperSlide>
+          ))
+        }
+       
       </Swiper>
       <Swiper
         onSwiper={setThumbsSwiper}
@@ -76,27 +67,13 @@ export default function App() {
           }
         }
       >
-        <SwiperSlide>
-          <img src="../images/jobs/slider/slider1.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="../images/jobs/slider/slider2.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="../images/jobs/slider/slider3.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="../images/jobs/slider/slider4.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="../images/jobs/slider/slider5.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="../images/jobs/slider/slider6.webp" alt='equipo mangobc' />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="../images/jobs/slider/slider7.webp" alt='equipo mangobc' />
-        </SwiperSlide>
+        {
+          sliders.map((img) => (
+            <SwiperSlide key={img.id}>
+              <img className='max-w-[800px]' src={img.image} alt='equipo mangobc' />
+            </SwiperSlide>
+          ))
+        }
       </Swiper>
     </>
   );
